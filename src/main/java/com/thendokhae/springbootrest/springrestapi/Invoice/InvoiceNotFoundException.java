851ac2cec427787383f94b0a1c043f5e20dd0e67
B2B\MakahaneT <thendo.makahane@reedbusiness.com>
2018-10-17 15:01:22 +0200

    added invoice repository interface and incorparated it into the invoice resource

---
+++ src/main/java/com/thendokhae/springbootrest/springrestapi/Invoice/InvoiceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.thendokhae.springbootrest.springrestapi.Invoice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InvoiceNotFoundException extends RuntimeException {
+
+    public InvoiceNotFoundException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/thendokhae/springbootrest/springrestapi/Invoice/InvoiceResource.java
+++ src/main/java/com/thendokhae/springbootrest/springrestapi/Invoice/InvoiceResource.java
@@ -1,27 +1,38 @@
 package com.thendokhae.springbootrest.springrestapi.Invoice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class InvoiceResource {
 
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
     @GetMapping("/invoices")
     public List<InvoiceEntity> viewAllInvoices(){
-        return new ArrayList<>();
+        return invoiceRepository.findAll();
     }
 
     @GetMapping("/invoices/{invoiceId}")
     public InvoiceEntity viewInvoice(@PathVariable Long invoiceId){
-        return new InvoiceEntity();
+        Optional<InvoiceEntity> invoice = invoiceRepository.findById(invoiceId);
+        if(!invoice.isPresent()){
+            throw  new InvoiceNotFoundException("Invoice with id : "+invoiceId+" does not exist!");
+        }
+        return invoice.get();
     }
 
     @PostMapping("/invoices")
     public InvoiceEntity addInvoice(@Valid @RequestBody InvoiceEntity invoiceEntity){
-        return invoiceEntity;
+        InvoiceEntity invoice =  invoiceRepository.save(invoiceEntity);
+
+        return invoice;
     }
 }
